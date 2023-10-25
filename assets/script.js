01a9616fc0cd7e4a5173982960a98127e7afc347
lexayart <roche.axelle@yahoo.fr>
2023-10-25 18:27:54 +0200

    FIX - changemt library, ajout filtres, opti tech

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,51 @@
-$("#nanogallery2").nanogallery2({
-  thumbnailHeight: 380,
-  thumbnailWidth: 380,
-  thumbnailL1BorderHorizontal: 0,
-  thumbnailL1BorderVertical: 0,
-  thumbnailL1GutterWidth: "20px",
-  thumbnailL1GutterHeight: "20px",
-  thumbnailLabel: { display: "false" },
-  galleryFilterTags: true,
-  galleryL1FilterTagsMode: "single",
-  thumbnailAlignment: "fillWidth",
-  galleryTheme: {
-    navigationFilter: { color: 'black', background: 'white', colorSelected: 'white', backgroundSelected: '#BEB45A', borderRadius: '0px' },
+import PhotoSwipeLightbox from './../node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js';
 
-  },
+const lightboxTous = new PhotoSwipeLightbox({
+  gallery: '#gallery-content',
+  children: 'a',
+  pswpModule: () => import('./../node_modules/photoswipe/dist/photoswipe.esm.min.js'),
 });
+lightboxTous.init();
+console.log(lightboxTous)
+
+
+
+const gallery = document.querySelector('#gallery-content')
+const galleryAll = document.querySelectorAll(".gallery-item")
+const Concert = document.querySelectorAll(".Concert")
+const Entreprises = document.querySelectorAll(".Entreprises")
+const Mariages = document.querySelectorAll(".Mariages")
+const Portrait = document.querySelectorAll(".Portrait")
+const filtersList = document.querySelectorAll(".filter-button")
+
+for (const filter of filtersList) {
+  filter.addEventListener("click", function () {
+    const activated = document.querySelector(".activated")
+    activated.classList.remove("activated")
+    filter.classList.add("activated")
+    if (filter.value === "Tous") {
+      for (const photo of galleryAll) {
+        photo.style.display = ""
+      }
+    } else {
+      for (const photo of galleryAll) {
+        photo.style.display = "none"
+      }
+      const wantedClass =filter.value
+      const filteredGallery = document.querySelectorAll("."+ wantedClass)
+      for(const filteredPhoto of filteredGallery){
+        filteredPhoto.style.display = ""
+
+        const lightboxFilter = new PhotoSwipeLightbox({
+          gallery: '#gallery-content',
+          children: filteredGallery,
+          pswpModule: () => import('./../node_modules/photoswipe/dist/photoswipe.esm.min.js'),
+        });
+        lightboxFilter.init
+        lightboxFilter.classList.add("lightboxFilter")
+
+      }
+    }
+  })
+}
+
